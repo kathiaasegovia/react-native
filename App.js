5d7a052cc869d6e96510adf681dfa4e8a9d2b7cd
KathiaSegovia <kathiasegovia@gmail.com>
2021-01-21 22:54:10 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+            <MapView style={styles.mapStyle}
+                  showCompass={true}
+                  region={{
+                             latitude:38.616639,
+                             longitude:-109.619835,
+
+                             latitudeDelta:.3,
+                             longitudeDelta:.3
+                           }}
+            />
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +31,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
