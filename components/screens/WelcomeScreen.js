fe85a8b8d381bc171ad7063b0ee807fc6ce7bce1
KathiaSegovia <kathiasegovia@gmail.com>
2021-02-08 23:03:03 -0700

    Project setup

---
+++ components/screens/WelcomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Welcome = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Welcome!</Text>
+    </View>
+  )
+}
+export default Welcome;
+
