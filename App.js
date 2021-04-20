c57193d6b3970b5a27cc8fe54d5cda44546d3fb9
Kathia A. Segovia <76544794+kathiaasegovia@users.noreply.github.com>
2021-04-19 23:04:59 -0600

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ export default class App extends React.Component {
   render() { 
     return (
       <View style={styles.container}>
-
             <MapView style={styles.mapStyle}
                   showCompass={true}
                   customMapStyle={MapStyles}
